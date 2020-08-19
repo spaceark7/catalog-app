355a8766c3828a66513dfc7685b31b8bc2e179ad
SpaceArk& <spacearkchief@gmail.com>
2020-08-19 20:26:40 +0700

    New Content Bar

--- src/components/Contents.jsx
+++ src/components/Contents.jsx
@@ -8,28 +8,42 @@ import image from "../image/DI4.png";
 
 AOS.init({ duration: 2000, delay: 400 });
 const Contents = () => {
+  // const project1 = document.getElementsByClassName("project1");
+  // project1.scrollIntoView({
+  //   behavior: "smooth",
+  //   block: "end",
+  //   inline: "nearest",
+  // });
   return (
     <div id="trigger-1" className="container-fluid">
-      <h1>Kenapa Harus Go DIgital?</h1>
+      <h1 data-aos="fade-right" id="Title">
+        Kenapa Harus Go Digital?
+      </h1>
 
       <div className="row ">
-        <div className="col-8">
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque optio
-            neque, iure quisquam dolorem similique pariatur quos? A omnis soluta
-            dolor perspiciatis, ullam quidem ipsum. Cupiditate, eveniet commodi.
-            Incidunt, ipsum.
+        <div className="col-7">
+          <p data-aos="fade-up-right" className="content-text1">
+            Pemasaran secara digital bisa didefinisikan sebagai semua upaya
+            pemasaran menggunakan perangkat elektronik/internet dengan beragam
+            taktik marketing dan media digital dimana Anda dapat berkomunikasi
+            dengan calon konsumen yang menghabiskan waktu di online. Ada beragam
+            akses untuk para calon konsumen agar dapat melihat penawaran Anda,
+            seperti Website, Blog, Media sosial{" "}
+            <strong>(Instagram, Whatsapp, Facebook, dsb).</strong> Dari beberapa
+            akses itulah mereka akan berkomunikasi kepada Anda.
           </p>
         </div>
         <div
-          data-aos="zoom-out-up"
+          data-aos="fade-left"
           data-aos-anchor="#trigger-1"
           data-aos-anchor-placement="top-center"
           className="col-4 project project1"
         ></div>
+        <div className="col-1"></div>
       </div>
-      <div style={{ height: 200 }}></div>
+      <div style={{ height: 150 }}></div>
       <div id="trigger-2"></div>
+
       <div className="row ">
         <div
           data-aos-anchor-placement="top-bottom"
@@ -37,12 +51,32 @@ const Contents = () => {
           data-aos-anchor="#trigger-2"
           className="col-6 project project2"
         ></div>
-        <div className="col-6"></div>
+        <div className="col-6">
+          <p data-aos="fade-up-left" className="content-text2">
+            Selain pandai memperhitungkan rugi dan untung. Sebagai seorang
+            pelaku bisnis, anda juga dituntut untuk berfikir secara kreatif agar
+            produk-produk anda dapat dilirik oleh masyarakat. Salah satu cara
+            untuk memasarkan produk tersebut adalah dengan memasang
+            <strong>
+              iklan yang menarik dan kekinian untuk postingan feed serta story
+              dimedia sosial anda
+            </strong>{" "}
+            Sayangnya sampai saat ini masih banyak pelaku UKM Indonesia yang
+            kerap menghindari atau melewatkan iklan begitu saja.
+          </p>
+        </div>
       </div>
-      <div style={{ height: 200 }}></div>
+      <div style={{ height: 150 }}></div>
       <div id="trigger-3"></div>
       <div className="row ">
-        <div className="col-7"></div>
+        <div className="col-7">
+          <p data-aos="fade-up-right" className="content-text3">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque optio
+            neque, iure quisquam dolorem similique pariatur quos? A omnis soluta
+            dolor perspiciatis, ullam quidem ipsum. Cupiditate, eveniet commodi.
+            Incidunt, ipsum.
+          </p>
+        </div>
         <div
           id="trigger-4"
           data-aos-anchor-placement="top-bottom"
@@ -51,7 +85,7 @@ const Contents = () => {
           className="col-4 project project3"
         ></div>
 
-        <div style={{ height: 200 }}></div>
+        <div style={{ height: 150 }}></div>
         <div id="trigger-3"></div>
         <div className="col-1"></div>
       </div>
